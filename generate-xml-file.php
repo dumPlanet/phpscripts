b2078fd9e84d0e74fb94a5d92cb06ad68686d949
requiescat in pace <109029067+dumPlanet@users.noreply.github.com>
2023-06-05 12:49:44 +0200

    Create generate-xml-file.php

---
+++ generate-xml-file.php
@@ -0,0 +1,33 @@
+function generateXmlFile() {
+    
+    if (!isset($_POST['generateXmlFile']) && !isset($_POST['xmlgen'])) {
+        return;
+    }
+    
+    if ($_POST['xmlgen'] == 'Yes') {
+            
+        //create new xml file
+        $dom = new SimpleXMLElement('<?xml version="1.0" encoding="utf-8"?><quotes></quotes>');
+
+        //database sql
+        $database = dbConnect(DBHOST, DBUSER, DBPASSWORD, DBNAME);
+        $sqlstring = "SELECT * FROM quotes";
+        $result = $database->query($sqlstring);
+        
+        //write into xml from sql
+        foreach($result as $key) {
+            $parentNode = $dom->addChild('quote');
+            $parentNode->addChild('id', $key['id']);
+            $parentNode->addChild('quotetext', $key['quotetext']);
+            $parentNode->addChild('author', $key['author']);
+            $parentNode->addChild('job', $key['job']);
+            $parentNode->addChild('advertising', $key['advertising']);
+            $parentNode->addChild('date', $key['date']);        
+        }
+        
+        //save to file
+        $dom->saveXML(CONTENTDB);
+    } else {
+        return;
+    }
+}
